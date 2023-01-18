c4701382e4c579c291e3a2dff42cbea99d29483e
Ronny Krämer <ronny.kraemer@uni-bremen.de>
2023-01-18 22:20:32 +0100

    fixed wrong uid

--- ext_localconf.php
+++ ext_localconf.php
@@ -4,4 +4,4 @@ if (!defined('TYPO3_MODE')) {
     die ('Access denied.');
 }
 
-//$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:cnslider/Configuration/RTE/cnslider.yaml';
+$GLOBALS['TYPO3_CONF_VARS']['RTE']['Presets']['default'] = 'EXT:cnslider/Configuration/RTE/cnslider.yaml';
